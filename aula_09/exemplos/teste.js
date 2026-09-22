119061fbb64e40a3b087741462eebbacfa3a3f5a
jaqueline0209bc <jaqueline.bc0209@gmail.com>
2026-09-22 19:20:31 +0000

    segunda atividade

---
+++ aula_09/exemplos/teste.js
@@ -0,0 +1,13 @@
+function verificarIdade(idade) {
+    if (idade === 30) {
+        console.log("Idade igual a 30");
+    }
+    else if (idade >=18) {
+        console.log("Maior de idade");
+    } 
+    else {
+        console.log("Menor de idade");
+    }
+}
+
+verificarIdade(30);
